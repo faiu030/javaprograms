4b2b3b3a60ab71642d6040aaa2819b813addb5bd
faiu030 <123292743+faiu030@users.noreply.github.com>
2023-07-11 20:37:33 +0530

    Update Calculator.java

--- calculator/src/Calculator.java
+++ calculator/src/Calculator.java
@@ -1,38 +1,4 @@
-import java.util.Scanner;
-
 public class Calculator {
-    public char calculation(char s)
-    {
-        Scanner n=new Scanner(System.in);
-        System.out.println("enter two numbers");
-        int num1=n.nextInt();
-        int num2=n.nextInt();
-        switch (s)
-        {
-            case '+':
-                System.out.println(addition(num1,num2));
-                break;
-            case '-':
-                System.out.println(subtraction(num1,num2));
-                break;
-            case '*':
-                System.out.println(multiplication(num1,num2));
-                break;
-            case '/':
-                System.out.println(division(num1,num2));
-                break;
-            case '%':
-                System.out.println(remainder(num1,num2));
-                break;
-            case '^':
-                System.out.println(square(num1));
-                break;
-            case '!':
-                System.out.println("Thank you");
-            break;
-        }
-        return null;
-    }
     public int addition(int num1,int num2)
     {
         return num1+num2;
